96b863e230f1ba2b770290ff44d0fd14988bbf46
Bernhard Kern <bkern@it-economics.de>
2018-03-15 14:16:57 +0100

    CassandraConfiguration

--- src/main/java/de/virtual7/reactivelabclient/config/CassandraConfiguration.java
+++ src/main/java/de/virtual7/reactivelabclient/config/CassandraConfiguration.java
@@ -1,11 +1,39 @@
 package de.virtual7.reactivelabclient.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by mihai.dobrescu
  */
 @Configuration
-public class CassandraConfiguration {
+public class CassandraConfiguration extends AbstractCassandraConfiguration {
 
+    @Override
+    protected String getKeyspaceName() {
+        return "reactive_lab";
+    }
+
+    @Override
+    protected String getContactPoints() {
+
+        //specify nodes here
+        return super.getContactPoints();
+    }
+
+    @Override
+    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+        final CreateKeyspaceSpecification specification =
+                CreateKeyspaceSpecification.createKeyspace("reactive_lab")
+                        .ifNotExists()
+                        .withSimpleReplication()
+                        .with(KeyspaceOption.DURABLE_WRITES);
+
+        return Collections.singletonList(specification);
+    }
 }
